fd69b73b4d982b5028579c810d823d94d9ca1f79
kfuquay <kirsty.fuquay@gmail.com>
2019-07-19 13:31:24 -0400

    use context to render li of plot names

--- src/routes/Dash/Dash.js
+++ src/routes/Dash/Dash.js
@@ -1,13 +1,23 @@
 import React, { Component, Fragment } from "react";
+import GardenContext from "../../context/GardenContext";
 
 class Dash extends Component {
+  static contextType = GardenContext;
+
   render() {
+    console.log(this.context.plots);
     return (
       <Fragment>
-        <ul>
-          <li>Veggie Plot</li>
-          <li>Flower Plot</li>
-        </ul>
+        <p>hi</p>
+        {this.context.plots ? (
+          <ul>
+            {this.context.plots.map(plot => {
+              return <li>{plot.plotName}</li>;
+            })}
+          </ul>
+        ) : (
+          <p>You don't have any saved Plots!</p>
+        )}
       </Fragment>
     );
   }
