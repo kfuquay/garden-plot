256292df30e147ed624ea9fd5e5fe8651544e3ab
kfuquay <kirsty.fuquay@gmail.com>
2019-07-19 11:11:45 -0400

    add basic styling to nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
+import "./Nav.css";
 
 class Nav extends Component {
   render() {
     return (
       <nav role="navigation">
-        <NavLink to="/">
-          <h1>Garden Plot</h1>
+        <NavLink to="/" className="NavLink">
+          <h1 className="header">Garden Plot</h1>
+        </NavLink>
+        <NavLink to="/login" className="NavLink">
+          Login
         </NavLink>
       </nav>
     );
